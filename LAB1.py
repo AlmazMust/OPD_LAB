36c45ade1ee829f1f83ee15778b208669366dec1
AlmazMust <Almaz08.09.2006@gmail.com>
2025-03-15 18:33:45 +0600

    Update LAB1.py

--- LAB1.py
+++ LAB1.py
@@ -5,4 +5,16 @@ url = 'http://www.omgtu.ru/general_information/faculties/'
 
 response = requests.get(url)
 
-if response.status_code == 200
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    faculty_list = soup.find('ul')
+    faculties = faculty_list.find_all('li')
+    with open('faculties.txt', 'w', encoding='utf-8') as file:
+        for faculty in faculties:
+            faculty_name = faculty.find('a').text.strip()
+            file.write(faculty_name + '\n')
+
+    print('Список факультетов сохранен в файл faculties.txt')
+else:
+    print(f"Не удалось получить страницу, код ответа: {response.status_code}")
+  
